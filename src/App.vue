8533f49e7c037006e6f09c94bb07115353ac0dbd
mleebs0282 <43883374+mleebs0282@users.noreply.github.com>
2020-11-22 18:49:16 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Commands title="CLI Commands"/>
+</template>
+
+<script>
+import Commands from './components/Commands.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Commands
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Commands.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="commands">
+    <h1>{{ title }}</h1>
+    <table>
+        <thead class="thead-dark">
+            <tr>
+                <th>Description</th>
+                <th>Usage</th>
+                <th>Platform</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="command in commands" :key="command.Id">
+                <td>{{command.description}}</td>
+                <td>{{command.usage}}</td>
+                <td>{{command.platform}}</td>
+            </tr>
+        </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        name: 'Commands',
+        data() {
+            return {
+                commands: []
+            }
+        },
+        mounted () {
+            axios
+                .get('https://localhost:5001/api/Command')
+                .then(response => (this.commands = response.data.commands))
+        },
+        props: {
+            title: String
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
